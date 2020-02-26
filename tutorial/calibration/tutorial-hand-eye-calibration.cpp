3c37fc4fa46f3f9a50a02af15fee8aa7161c1a88
Jeff Elms <jeffrey.elms@visualrobotics.com>
2020-02-26 14:15:17 -0800

    Add sanity check to make sure I understand what the true answer should be.
    By having a pre.yaml file with the assume answer, we can compute the
    world pose of the calibration target and print them to see how
    consistent it is using the apriori extrinsic ePc pose.

--- tutorial/calibration/tutorial-hand-eye-calibration.cpp
+++ tutorial/calibration/tutorial-hand-eye-calibration.cpp
@@ -22,6 +22,22 @@ int main(int argc, char *argv[])
   std::vector<vpHomogeneousMatrix> wMe(ndata);
   vpHomogeneousMatrix eMc;
 
+  vpPoseVector start(0,0.1,0,3.1415,0,0);
+  vpPoseVector rel(0.1,0.1,0.1,2.22,2.22,0);
+
+  vpHomogeneousMatrix start_h = vpHomogeneousMatrix(start);
+  vpHomogeneousMatrix rel_h =  vpHomogeneousMatrix(rel);
+  vpHomogeneousMatrix res_h = start_h * rel_h;
+
+  std::cout << "res_h: " << res_h << std::endl;
+  std::cout << "res: " << vpPoseVector(res_h) << std::endl;
+
+  vpHomogeneousMatrix res2_h = rel_h * start_h;
+
+  std::cout << "res2_h: " << res2_h << std::endl;
+  std::cout << "res2: " << vpPoseVector(res2_h) << std::endl;
+  
+
   for (unsigned int i = 1; i <= ndata; i++) {
     std::ostringstream ss_fPe, ss_cPo;
     ss_fPe << "pose_fPe_" << i << ".yaml";
@@ -43,8 +59,21 @@ int main(int argc, char *argv[])
     cMo[i-1] = vpHomogeneousMatrix(cPo);
   }
 
-  vpHandEyeCalibration::calibrate(cMo, wMe, eMc);
+ 
+  int ret = vpHandEyeCalibration::calibrate(cMo, wMe, eMc);
 
+  vpPoseVector pre;
+  std::string pre_n = "pre.yaml";
+  if (pre.loadYAML(pre_n, pre)  == false) {
+      std::cout << "Unable to read data from: " << pre_n << std::endl;
+  } else {
+    vpHomogeneousMatrix preh(pre);
+    for (unsigned int i = 0; i <= ndata - 1; i++) {
+      std::cout << "res " << i << ":" << vpPoseVector(wMe[i] * preh * cMo[i]) << std::endl;
+    }
+  }
+
+  
   // save eMc
   std::ofstream file_eMc("eMc.txt");
   eMc.save(file_eMc);
@@ -53,7 +82,7 @@ int main(int argc, char *argv[])
   std::cout << "\nSave eMc.yaml" << std::endl;
   pose_vec.saveYAML("eMc.yaml", pose_vec);
 
-  std::cout << "\nOutput: Hand-eye calibration result: eMc estimated " << std::endl;
+  std::cout << "\nOutput: Hand-eye calibration result " << ret << " : eMc estimated " << std::endl;
   std::cout << eMc << std::endl;
   vpThetaUVector ePc(eMc);
   std::cout << "theta U (deg): " << vpMath::deg(ePc[0]) << " " << vpMath::deg(ePc[1]) << " " << vpMath::deg(ePc[2]) << std::endl;
